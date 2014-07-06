96195127507f931c5fb0e1a75029452c0a35c9d9
timchen86 <tim.chen.86@gmail.com>
2014-07-07 01:08:28 +0800

    use ffmpeg instead of avconv, due to -ss issue

--- movsnap.py
+++ movsnap.py
@@ -1,13 +1,13 @@
 #/usr/bin/env python
+import subprocess
+import pipes
+import os
 import re
 import sys
-#convert z.jpeg -pointsize 70 -gravity south -stroke '#000C' -strokewidth 2 -annotate 0 'Hello Red\nMist' -stroke  none -fill white -annotate 0 'Hello Red\nMist' 
 
-# snap video to png from subtitle time
-if len(sys.argv) != 3:
-    print "usage: %s sub_file video_file" % sys.argv[0]
+if len(sys.argv) != 4:
+    print "usage: %s sub_file video_file n" % sys.argv[0]
     sys.exit(1)
-
 
 # subtitle file
 sub_file = sys.argv[1]
@@ -23,21 +23,31 @@ video_file = sys.argv[2]
 # grab time offset from subtitle file
 with open(sub_file, "r") as f:
     sub_content = f.read()
-    #re_sub = re.findall("([\d]+)[\r]*$\n([\d:,]+)\s+-->\s+[\d:,]+[\r]*$\n(.*?)^[\r\n]*$",sub_content,re.S|re.M)
-    #re_sub = re.findall("([\d]+)[\r]*$\n([\d]+):([\d]+):([\d]+),\d+\s+-->\s+[\d:,]+[\r]*$\n(.*?)^[\r\n]*$",sub_content,re.S|re.M)
-    subs_raw = re.findall("[\d]+[\r]*$\n([\d]+):([\d]+):([\d]+),\d+\s+-->\s+[\d:,]+[\r]*$\n(.*?)^[\r\n]*$",sub_content,re.S|re.M)
-
+    subs_raw = re.findall("[\d]+[\r]*$\n([\d]+):([\d]+):([\d]+),(\d+)\s+-->\s+[\d:,]+[\r]*$\n(.*?)^[\r\n]*$",sub_content,re.S|re.M)
 
 
 subs = []
-for h, m, s, sub in subs_raw:
-    subs.append((int(h)*3600+int(m)*60+int(s), sub))
+for h, m, s, ms, sub in subs_raw:
+    sub = re.sub("<.*?>","",sub)
+    t = "%s:%s:%s.%s" % (h, m, s, ms)
+    t_x = "%s-%s-%s.%s" % (h, m, s, ms)
+    subs.append((t, t_x, sub))
 
 print subs
 
+base_dir = os.path.dirname(video_file)
+tmp_dir = os.path.join(base_dir, "movsnap_tmpdir")
+try:
+    os.makedirs(tmp_dir)
+except:
+    pass
 
-# video to png
-# avconv -ss 4000 -i Kick\ Ass\ 2\ \(2013\)/Kick.Ass.2.1080p.5.1.2013.BluRay.mp4  -vframes 1 out.jpeg
-# 
+for time, time_x, sub in subs[0:n]:
+    out_file = os.path.join(tmp_dir, "%s.png" % time_x) 
+    ffmpeg_exe = "ffmpeg -y -ss %s -i \"%s\" -vframes 1 \"%s\"" % (time, (video_file), (out_file))
+    print ffmpeg_exe
+    subprocess.check_call(ffmpeg_exe, shell=True)
+    convert_exe = "convert \"%s\" -pointsize 70 -gravity south -stroke '#000C' -strokewidth 2 -annotate 0 \"%s\" -stroke  none -fill white -annotate 0 \"%s\" \"%s\"" % ((out_file), (sub), (sub), (out_file))
 
-
+    print convert_exe
+    subprocess.check_call(convert_exe, shell=True)
