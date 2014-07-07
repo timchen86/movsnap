12d36fba54aa95212f4cd9f31ba1c3a2701c30e3
timchen86 <tim.chen.86@gmail.com>
2014-07-07 10:40:34 +0800

    make text bold, pipes.quote for shell string

--- movsnap.py
+++ movsnap.py
@@ -5,8 +5,8 @@ import os
 import re
 import sys
 
-if len(sys.argv) != 4:
-    print "usage: %s sub_file video_file n" % sys.argv[0]
+if len(sys.argv) != 5:
+    print "usage: %s sub_file video_file start end" % sys.argv[0]
     sys.exit(1)
 
 # subtitle file
@@ -15,15 +15,16 @@ sub_file = sys.argv[1]
 # video file
 video_file = sys.argv[2]
 
-# n
-n = int(sys.argv[3])
+# 
+start = int(sys.argv[3])
+end = int(sys.argv[4])
 
 """
 11
 00:01:29,530 --> 00:01:32,124
 <i>Every weekday, for 12 years...</i>
 """
-# grab time offset from subtitle file
+# grab time offset and text from subtitle file
 with open(sub_file, "r") as f:
     sub_content = f.read()
     subs_raw = re.findall("[\d]+[\r]*$\n([\d]+):([\d]+):([\d]+),(\d+)\s+-->\s+[\d:,]+[\r]*$\n(.*?)^[\r\n]*$",sub_content,re.S|re.M)
@@ -40,17 +41,30 @@ print subs
 
 base_dir = os.path.dirname(video_file)
 tmp_dir = os.path.join(base_dir, "movsnap_tmpdir")
+
 try:
     os.makedirs(tmp_dir)
 except:
     pass
 
-for time, time_x, sub in subs[0:n]:
+video_file = pipes.quote(video_file)
+for time, time_x, sub in subs[start:end]:
     out_file = os.path.join(tmp_dir, "%s.png" % time_x) 
-    ffmpeg_exe = "ffmpeg -y -ss %s -i \"%s\" -vframes 1 \"%s\"" % (time, (video_file), (out_file))
+    out_file = pipes.quote(out_file)
+
+    ffmpeg_exe = "ffmpeg -y -ss %s -i %s -vframes 1 %s" % (time, (video_file), (out_file))
     print ffmpeg_exe
-    subprocess.check_call(ffmpeg_exe, shell=True)
-    convert_exe = "convert \"%s\" -pointsize 70 -gravity south -stroke '#000C' -strokewidth 2 -annotate 0 \"%s\" -stroke  none -fill white -annotate 0 \"%s\" \"%s\"" % ((out_file), (sub), (sub), (out_file))
+
+    try:
+        subprocess.check_call(ffmpeg_exe, shell=True)
+    except:
+        pass
+
+    sub_txt = pipes.quote(sub)
+    convert_exe = "convert %s -pointsize 70 -gravity south -stroke black -strokewidth 20 -annotate 0 %s -stroke none -fill white -annotate 0 %s %s" % ((out_file), (sub_txt), (sub_txt), (out_file))
 
     print convert_exe
-    subprocess.check_call(convert_exe, shell=True)
+    try:
+        subprocess.check_call(convert_exe, shell=True)
+    except:
+        pass
